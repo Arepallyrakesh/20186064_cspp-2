97e3bf5c50dde413f54c6ed754b6f08e8106f7b0
Arepallyrakesh <rk.rakhi514@msitprogram.net>
2018-08-27 19:45:57 +0530

    degrees

---
+++ cspp1 practice/m1/Degrees_to_fahrenheit.java
@@ -0,0 +1,20 @@
+/** Deg.java.
+ * @author Arepallyrakesh.
+ */
+public final class Degrees_to_fahrenheit {
+    /**
+     *empty constructor.
+     */
+    private Degrees_to_fahrenheit() {
+
+    }
+    /**
+     * main function to print the hello murthy.
+     * @param args is the parameter for this function
+     */
+    public static void main(final String[] args) {
+        int a = 40;
+        double f = ((a * 1.8) + 32);
+        System.out.println(f);
+    }
+}
