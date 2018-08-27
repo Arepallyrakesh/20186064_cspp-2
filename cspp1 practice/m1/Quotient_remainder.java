e0ec0ccc7772872c2ffed582f14119fd58dfba76
Arepallyrakesh <rk.rakhi514@msitprogram.net>
2018-08-27 19:30:35 +0530

    quotient

---
+++ cspp1 practice/m1/Quotient_remainder.java
@@ -0,0 +1,22 @@
+/** swap.java.
+ * @author Arepallyrakesh.
+ */
+public final class Quotient_remainder {
+    /**
+     *empty constructor.
+     */
+    private Quotient_remainder() {
+
+    }
+    /**
+     * main function to print the hello murthy.
+     * @param args is the parameter for this function
+     */
+    public static void main(final String[] args) {
+        int a = 6;
+        int b = 2;
+        int c = a / b;
+        int d = a % b;
+        System.out.println(c + " " + d);
+    }
+}
