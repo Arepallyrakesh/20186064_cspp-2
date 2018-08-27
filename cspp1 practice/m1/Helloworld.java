5b0b90593889b10f2755e0d5c768c8b61921176b
Arepallyrakesh <rk.rakhi514@msitprogram.net>
2018-08-27 18:41:55 +0530

    helloworld

---
+++ cspp1 practice/m1/Helloworld.java
@@ -0,0 +1,18 @@
+/** Helloworld.java.
+ * @author Arepallyrakesh.
+ */
+public final class Helloworld {
+    /**
+     *empty constructor.
+     */
+    private Helloworld() {
+
+    }
+    /**
+     * main function to print the hello murthy.
+     * @param args is the parameter for this function
+     */
+    public static void main(final String[] args) {
+        System.out.println("Hello !!");
+    }
+}
