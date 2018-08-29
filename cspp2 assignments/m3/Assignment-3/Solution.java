cfed48a39e77422fe58121348e1f06686f3345f4
Arepallyrakesh <rk.rakhi514@msitprogram.net>
2018-08-29 18:02:20 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- cspp2 assignments/m3/Assignment-3/Solution.java
+++ cspp2 assignments/m3/Assignment-3/Solution.java
@@ -1,20 +1,30 @@
-
 import java.util.Scanner;
-/*
-	Do not modify this main function.
-	*/
-public class Solution {
+/**
+ *do not modify this main function.
+ * @author Supriya
+ */
+public final class Solution {
+    /**
+    *Private constructor.
+    */
+    private Solution() {
+    //We are not using this constructor.
+    }
+     /**
+     * { function_description }.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
 
-    public static void main(String[] args) {
 
-        Scanner s=new Scanner(System.in);      
+        Scanner s = new Scanner(System.in);
         int n1 = s.nextInt();
         int n2 = s.nextInt();
-        System.out.println((int)gcd(n1,n2));
+        System.out.println((int) gcd(n1, n2));
     }
-    
     /**
-     * { function_description }
+     * { function_description }.
      *
      * @param      n1    The n 1
      * @param      n2    The n 2
@@ -23,8 +33,8 @@ public class Solution {
      */
     public static int gcd(final int n1, final int n2) {
         int gcd = 0;
-        for(int i = 1; i <= n1 && i <= n2; i++) {
-            if(n1 % i == 0 && n2 % i == 0 ) {
+        for (int i = 1; i <= n1 && i <= n2; i++) {
+            if (n1 % i == 0 && n2 % i == 0) {
               gcd = i;
               /*return gcd;*/
             }
