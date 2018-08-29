12d8ceb7741b28906fdaf879bbb9581749b58f88
Arepallyrakesh <rk.rakhi514@msitprogram.net>
2018-08-29 17:53:49 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

---
+++ cspp2 assignments/m3/Assignment-2/Solution.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+/**
+ *do not modify this main function.
+ * @author Supriya
+ */
+public final class Solution {
+    /**
+     * Private constructor.
+     */
+    private Solution() {
+    //We are not using this constructor.
+    }
+    /**
+    *@variable TEN variable.
+    */
+    private static final int TEN = 10;
+    /**
+    *@variable SEVEN variable.
+    */
+    private static final int SEVEN = 7;
+     /**
+      * { function_description }.
+      *
+      * @param      args  The arguments
+      */
+    public static void main(final String[] args) {
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        int count = 0;
+        for (int i = 1; i < n; i++ ) {
+            int z = i;
+            while(z != 0) {
+                int k = z % TEN;
+                z = z / TEN;
+                if (k == SEVEN) {
+                    count += 1;
+                }
+            }
+        }
+        System.out.println(count);
+    }
+}
+
