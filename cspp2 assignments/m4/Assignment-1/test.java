5a57d17699f82e264c9050ed5aa216b3e370af30
Arepallyrakesh <rk.rakhi514@msitprogram.net>
2018-08-30 14:23:00 +0530

    "check_style_score -> 0 of 4 passed. style score: 0/1 "

---
+++ cspp2 assignments/m4/Assignment-1/test.java
@@ -0,0 +1,27 @@
+import java.util.*;
+public class test
+{/*
+	Fill this main function to print maximum of given array
+	*/
+	public static void main(String[] args)
+	{
+		/*int[] a = {10, 18, 30, 40, 50};
+		int[] b = {10, 18, 30, 40, 50};
+		if (a == b){
+			System.out.println("n");
+		}else{
+			System.out.println(a);
+			System.out.println(b);*/
+		int[] a = new int[-2];
+		a[0] = 0;
+		a[-1] = -1;
+		System.out.println(a[-1]);
+		}
+		/*int[] a = new int[2];
+		for (int i =0; i < a.length; i += 2){
+			System.out.println(a[i]);
+			System.out.println(a[2]);*/
+		}
+	
+
+
