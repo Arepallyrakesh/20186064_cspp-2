857d28c39b1b8f381b80f9e4b81490c1620df8a6
Arepallyrakesh <rk.rakhi514@msitprogram.net>
2018-08-31 17:49:50 +0530

    "check_style_score -> 1 of 1 passed. style score: 0/1 "

--- cspp2 assignments/m4/Assignment-3/Solution.java
+++ cspp2 assignments/m4/Assignment-3/Solution.java
@@ -10,7 +10,7 @@ public final class Solution {
      */
     private Solution() {
     }
-    /*
+    /**.
      * Do not modify this main function.
      *
      * @param      args  The arguments
@@ -21,12 +21,12 @@ public final class Solution {
         int n = sc.nextInt();
         for (int i = 0; i < n; i++) {
             String s = sc.next();
-            String res = binaryToDecimal(s);//Write binaryToDecimal function
+            String res = binaryToDecimal(s);
             System.out.println(res);
         }
     }
-    /**
-     * { function_description }
+    /**.
+     * { function_description }.
      *
      * @param      s     { parameter_description }
      *
@@ -34,11 +34,11 @@ public final class Solution {
      */
     public static String binaryToDecimal(final String s) {
         double j = 0;
-        for (int i = 0; i < s.length(); i++) { 
+        for (int i = 0; i < s.length(); i++) {
             if (s.charAt(i) == '1') {
-                j = j + Math.pow(2 , s.length() - 1 - i);
+                j = j + Math.pow(2, s.length() - 1 - i);
             }
         }
-        return Integer.toString ((int)j);
+        return Integer.toString ((int) j);
     }
 }
