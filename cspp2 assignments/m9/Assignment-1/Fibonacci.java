647dc10074959424ec56b2566c3ae672cf3be1f6
Arepallyrakesh <rk.rakhi514@msitprogram.net>
2018-09-05 17:18:56 +0530

    "check_style_score -> 2 of 4 passed. style score: 0/1 "

--- cspp2 assignments/m9/Assignment-1/Fibonacci.java
+++ cspp2 assignments/m9/Assignment-1/Fibonacci.java
@@ -46,7 +46,7 @@ public class Fibonacci
         fibonacci.add(n2);
         for (int i = 2; i < n; i++ ) {
             sum = n1 + n2;
-            fibonacci.add(n1+n2);
+            fibonacci.add(sum);
             n1 = n2;
             n2 = sum;
             
