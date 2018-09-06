6eac5da7f4d8336db77bbf5fae09c43abfea0aa3
Arepallyrakesh <rk.rakhi514@msitprogram.net>
2018-09-06 19:13:29 +0530

    "check_style_score -> 3 of 3 passed. style score: 0/1 "

--- cspp2 assignments/m10/Assignment-1/List.java
+++ cspp2 assignments/m10/Assignment-1/List.java
@@ -159,7 +159,7 @@ public class List {
      * Create a method called resize(). Resize should create an new array that is
      * double the size of the old array.
      * Then copy the contents of the old array to the new one.
-     * 
+     *
      * When should the resize method be invoked and from where?
      * Will the client invoke resize or is it internal to List class?
      * Should the resize be public method or private?
@@ -174,7 +174,6 @@ public class List {
      * The size method returns the value of the size.
      * The purpose of the method is to announce the size of the list
      * to the objects outside the list
-     * 
      * The method returns an int. Empty list should return 0.
      */
 
@@ -191,7 +190,7 @@ public class List {
      * The remove method does what the name suggests.
      * Removes an int item, specified by the index argument, from the list
      * It also does an additional step.
-     * Think about what happens when 
+     * Think about what happens when
      * an item is removed from the middle of the list
      * It creates a hole in the list, right?
      * This would mean, all the items that are
@@ -213,11 +212,11 @@ public class List {
      *
      * @param      index  The index
      */
-    public void remove(int index) {
+    public void remove(final int index) {
         // write the logic for remove here.
         // Think about what to do to the size variable.
-        if(index >= 0 && index < size) {
-            for(int i = index; i < size - 1; i++) {
+        if (index >= 0 && index < size) {
+            for (int i = index; i < size - 1; i++) {
                 list[i] = list[i + 1];
             }
             size--;
@@ -234,7 +233,7 @@ public class List {
      * How can an element not be there at a given position?
      * Well, if the position is greater than the number of items
      * in the list then that would mean the item doesn't exist.
-     * How do we check if the position is greater than the 
+     * How do we check if the position is greater than the
      * number of items in the list? Would size variable be useful?
      */
 
@@ -245,8 +244,8 @@ public class List {
      *
      * @return     { description_of_the_return_value }
      */
-    public int get(int index) {
-        if(index < 0 || index >= size) {
+    public int get(final int index) {
+        if (index < 0 || index >= size) {
             return -1;
         } else {
             return list[index];
@@ -262,7 +261,6 @@ public class List {
      * System.out.println(l);
      * This statement is a shortcut for
      * System.out.println(l.toString());
-     * 
      * So, implement the toString method to display the items
      * in the list in the square brackets notation.
      * i.e., if the list has numbers 1, 2, 3
@@ -280,11 +278,11 @@ public class List {
      * @return     String representation of the object.
      */
     public String toString() {
-        if(size == 0)
+        if (size == 0)
             return "[]";
         String str = "[";
         int i = 0;
-        for(i = 0; i < size - 1; i++) {
+        for (i = 0; i < size - 1; i++) {
             str = str + list[i] + ",";
         }
         str = str + list[i] + "]";
@@ -293,7 +291,6 @@ public class List {
             }
         return str;
     }
-    
     /**
      * Contains return true if the list has
      * the item passed as an argument to the method
@@ -323,8 +320,9 @@ public class List {
     public int indexOf(final int item) {
 
         for (int i = 0; i < size; i++) {
-            if (item == list[i])
+            if (item == list[i]){
                 return i;
+            }
         }
         return -1;
     }
@@ -353,7 +351,6 @@ public class List {
      /**
       Inserts the specified element at the specified index by moving all the
       elements to the right. The method returns void (nothing)
-     
       @param      index  The index
       @param      item   The item
      */
